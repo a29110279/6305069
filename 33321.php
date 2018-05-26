c5bcf5c99a73a417c1bdd9d0ff0e049d9c19b542
a29110279 <c29110279@gmail.com>
2018-05-26 16:00:01 +0800

    GGT

---
+++ 33321.php
@@ -0,0 +1,10 @@
+<html>
+	<body>
+	<h2>表單資料 : </h2>
+	<form action="post.php" method="POST">
+	<input type="text" name="account" placeholder="帳號">
+	<input type="password" name="password" placeholder="密碼">
+	<input type="submit">
+	</form>
+	</body>
+	</html>
