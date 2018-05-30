7736d0433911d36b177423fc3afc389839d733c2
a29110279 <c29110279@gmail.com>
2018-05-30 17:41:52 +0800

    1

--- 33321.php
+++ 33321.php
@@ -1,7 +1,7 @@
 <html>
 	<body>
 	<h2>表單資料 : </h2>
-	<form action="post.php" method="POST">
+	<form action="321.php" method="POST">
 	<input type="text" name="account" placeholder="帳號">
 	<input type="password" name="password" placeholder="密碼">
 	<input type="submit">
